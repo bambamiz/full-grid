eb0b8dcdc0096be2f20ab880c237faebb772b6ea
bambamiz <bambam-24@live.com>
2022-04-13 16:35:18 -0400

    added the whole project. got the grid working. color is being set upon hoverin, but it's setting it to the whole class, not just the one i'm hovering over.

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const pixs = document.querySelector("#pixsbutton");
+const pixsbutton = document.querySelector("#pixsbutton");
+const container = document.querySelector("#container");
 let gridsq = null;
+const grid = [];
+
+
 
 function asign() {
     gridsq = document.querySelector("#pixs")
+    document.getElementsByTagName("body").innerHTML = gridsq.value;
     
-
     for (let i = 0; i < gridsq.value; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row)
+        for (let n = 0; n < gridsq.value; n++) {
+            const square = document.createElement("div");
+            square.classList.add("box");
+            row.appendChild(square)
+
+            square.addEventListener("mouseover", function () {
+                color();
+            });
+            
+        }
         console.log(i);
     }
-
-    document.getElementById("container").innerHTML = gridsq.value;
+    
 }
 
-pixs.addEventListener("click", function() {
+pixsbutton.addEventListener("click", function() {
     asign();
 });
+
+function color() {
+    const collection = document.getElementsByClassName("box");
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.backgroundColor = "red";
+    }
+}
